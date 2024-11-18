33d9a57c893ac081eba3f584aeb6fc267016154d
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-11-18 16:28:05 +0100

    nixos/tcpdump: init
    (cherry picked from commit eb42ef0c24564082056a5ae54cdf52902bda58fd)

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -297,6 +297,7 @@
   ./programs/sysdig.nix
   ./programs/system-config-printer.nix
   ./programs/systemtap.nix
+  ./programs/tcpdump.nix
   ./programs/thefuck.nix
   ./programs/thunar.nix
   ./programs/thunderbird.nix
